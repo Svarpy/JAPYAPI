45e50ca97937e9949c907fbf11516a7b32e6c08e
Yashashchandra Kollu <yashashchandra25@gmail.com>
2022-09-20 00:14:27 +0530

    Create Spring Boot Starter Project with Spring Web Dependency

---
+++ src/test/java/org/svarpy/japi/JapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svarpy.japi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
